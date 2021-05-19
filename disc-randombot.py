107bf4f7b561034e1a14a0b89796bc0d041dee96
yuya mistui <copcop77@gmail.com>
2021-05-19 19:24:38 +0900

    fix

--- disc-randombot.py
+++ disc-randombot.py
@@ -20,7 +20,6 @@ async def on_ready():
 
 @tasks.loop(seconds=60)
 async def loop():
-    await message.channel.send(random.choice(randlist))
     now = datetime.now().strftime('%H:%M')
     yt = [y,t]
     if now == '19:25':
